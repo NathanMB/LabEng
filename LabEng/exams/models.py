6734db0021c8b4a7ffa25bb1e158866a5c03572a
harrison-santos <harrisonspeed1997@gmail.com>
2020-04-03 19:47:25 -0300

    Adição do modelo exam e report

--- LabEng/exams/models.py
+++ LabEng/exams/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Patient(models.Model):
@@ -18,9 +19,30 @@ class Patient(models.Model):
     sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=False, null=False)
     ethinicity = models.CharField(max_length=10, choices=ETHINICITY_CHOICES, blank=False, null=False)
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
-class Exams(models.Model):
-    pass
+class Exam(models.Model):
+    EXAM_TYPE_CHOICES = [
+        ('Ecocardiograma', 'Ecorcardiograma'),
+        ('Electrocardiograma', 'Electrocardiograma'),
+        ('Mapa', 'Mapa'),
+        ('Holter', 'Holter')
+    ]
+    doctor = models.ForeignKey(get_user_model(), blank=False, null=False, on_delete=models.CASCADE)
+    patient = models.ForeignKey('Patient', blank=False, null=False,on_delete=models.CASCADE)
+    appointment_date = models.DateField(null=False)
+    recommendations = models.CharField(max_length=300)
+    exam_type = models.CharField(max_length=20, choices=EXAM_TYPE_CHOICES, blank=False, null=False)
 
-class Reports(models.Model):
-    pass
+
+class Report(models.Model):
+    exam = models.ForeignKey('Exam', blank=False, null=False, on_delete=models.CASCADE)
+    #resident = models.ForeignKey('Resident', blank=False, null=False, on_delete=models.CASCADE)
+    approved = models.BooleanField()
+    realization_date = models.DateField(null=False)
+    diagnostic = models.CharField(max_length=300)
+
+    """def get_upload_handler(instance, filename):
+        return os.path.join('nomemedico', 'reports', 'nomearquivo')
+    file = models.ImageField(null=False)"""
